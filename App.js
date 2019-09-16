e4cd6f96dea65c85f43f7b189b4e94ab47eecc64
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-09-16 17:43:41 -0500

    created drawernav with the home stack nav as its initial route in the drawerRouterConfig

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { DrawerNavigator, DrawerItems } from "react-navigation";
+import HomeStackNav from "components/navigation/home-stack-navigator";
+import { COLORS } from "constants/styles";
+import styled from "styled-components";
+
+const DrawerContainer = styled.View`
+  flex: 1;
+  background-color: ${COLORS.GREY.BRIGHT_GREY};
+`;
+
+const AppContainer = styled.View`
+  flex: 1;
+  background-color: ${COLORS.GREY.BLACK_RUSSIAN};
+`;
+
+const drawerRouteConfig = {
+  Home: {
+    screen: HomeScreen
+  }
+};
+
+const CustomDrawerContentComponent = props => (
+  <DrawerContainer>
+    <DrawerItems {...props} />
+  </DrawerContainer>
+);
+
+const drawerNavConfig = {
+  contentComponent: props => <CustomDrawerContentComponent {...props} />
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <AppContainer>
+      <StatusBar hidden={true} />
+      <AppDrawer />
+    </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
